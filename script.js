b482b2bdf59a4c824ac22c679f1c92a11d9211c5
YBoah <yawboahene.yb@gmail.com>
2023-09-19 17:34:03 +0000

    Added more functionalities in the html & Js files

--- script.js
+++ script.js
@@ -11,3 +11,7 @@ async function getcode(url){
 }
 
 getcode(api_url);
+
+function tweet(){
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "---- by " + author.innerHTML, "Tweet Window", "width=600, height=300");
+}
