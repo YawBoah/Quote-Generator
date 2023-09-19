f7aea6d1b5bbda258c8d5d0a981d80df6746a488
YBoah <yawboahene.yb@gmail.com>
2023-09-19 17:18:01 +0000

    More functionalities

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
 const api_url = "https://api.quotable.io/random"
 
 async function getcode(url){
    const response = await fetch(url);
    var data = await response.json();
-   console.log(data);
+   
+   quote.innerHTML = data.content;
+   author.innerHTML = data.author;
 }
 
 getcode(api_url);
